f7be1d80a02130020815acb3fcf55fd549f91757
Hércules Ramlow <63937242+HRsniper@users.noreply.github.com>
2021-07-01 23:31:19 -0400

    day 2

--- react/letmeask/src/App.tsx
+++ react/letmeask/src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
+import { Home, NewRoom } from "./pages";
+import { AuthContextProvider } from "./contexts/AuthContext";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ react/letmeask/src/pages/index.ts
@@ -0,0 +1,4 @@
+import { Home } from "../pages/Home";
+import { NewRoom } from "../pages/NewRoom";
+
+export { Home, NewRoom };
